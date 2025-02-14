56c01a1cc7df314739fb92e0e524e41fd085636b
BandiKantiChaitanya <bandikantichaitanya2@gmail.com>
2025-02-14 16:20:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+function findLength(url) {
+	let n=url.length
+	return `The length of the URL is: ${n}`
+}
+const input = prompt("Enter a url"); 
+alert(findLength(input)); 
